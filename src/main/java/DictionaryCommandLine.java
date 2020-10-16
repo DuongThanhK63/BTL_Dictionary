0ae2c998d8fffd58e532598c8d74bd5bac770efa
DuongThanhK63 <duongthanhk63uet.com>
2020-10-16 23:57:11 +0700

    add main commandline + main DictFX

--- src/main/java/DictionaryCommandLine.java
+++ src/main/java/DictionaryCommandLine.java
@@ -18,12 +18,11 @@ public class DictionaryCommandLine {
                 "1, import -- load data from file." + "\n" +
                 "2, lookup -- search Word in the Dictionary" + "\n" +
                 "3, add -- add Words to the Dictionary" + "\n" +
-                "4, search -- search Words whose meaning is nearby the word you type in" + "\n" +
-                "5, save -- save Dictionary to file" + "\n" +
-                "6, quit" + "\n" +
-                "7, speech -- speech the word" + "\n" +
-                "8, delete -- delete a Word" + "\n" +
-                "9, type any to show the Dictionary data.!");
+                "4, save -- save Dictionary to file" + "\n" +
+                "5, quit" + "\n" +
+                "6, speech -- speech the word" + "\n" +
+                "7, delete -- delete a Word" + "\n" +
+                "8, type any to show the Dictionary data.!");
         Boolean check = true;
         String x;
         while(check == true){
@@ -32,6 +31,7 @@ public class DictionaryCommandLine {
             switch(x){
                 case "import": dict.insertFromFile();
                     break;
+
                 case "lookup": System.out.println("Nhap tu can tim: ");
                     String word = sc.nextLine();
                     String meaning = dict.dictionaryLookup(word);
@@ -41,6 +41,7 @@ public class DictionaryCommandLine {
                         System.out.println("Not Found!");
                     }
                     break;
+
                 case "add": int num;
                     System.out.println("Nhap so luong tu can them: ");
                     num = Integer.parseInt(sc.nextLine());
@@ -52,26 +53,24 @@ public class DictionaryCommandLine {
                         dict.addDict(a, b);
                     }
                     break;
+
                 case "quit": check = false;
                     break;
-                case "search": System.out.println("Tu can tim gan dung: ");
-                    String word1 = new String();
-                    word1 = sc.nextLine();
-                    ArrayList lWord = dict.searchNearbyWord(word1);
-                    if(lWord.isEmpty() != true){
-                        System.out.println(lWord);
-                    }
-                    break;
+
                 case "save": dict.saveFile();
                     System.out.println("Done!");
+                    break;
+
                 case "speech": System.out.println("Tu can phat am: ");
                     String a = sc.nextLine();
                     dict.speech(a);
                     break;
+
                 case "delete": System.out.println("Tu can xoa: ");
                     String b = sc.nextLine();
                     dict.removeWord(b);
                     break;
+
                 default: dict.showAllWord();
                     break;
             }
